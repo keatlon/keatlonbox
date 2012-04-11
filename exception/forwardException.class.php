f5e5668362f4ac782793ba06f6c32468f3e49455
Stanislav Starcha <stanislav.starcha@gmail.com>
2012-04-11 20:09:00 +0300

    extended forward exception

--- exception/forwardException.class.php
+++ exception/forwardException.class.php
@@ -1,9 +1,20 @@
 <?
 class forwardException extends applicationException
 {
-	public function __construct( $module, $action)
+	public function __construct( $module, $action, $method = false, $renderer = false)
 	{
-            $this->module = $module;
-            $this->action = $action;
+		$this->module = $module;
+		$this->action = $action;
+
+		if ($method)
+		{
+			request::method($method);
+		}
+
+		if ($renderer)
+		{
+			application::setRenderer($renderer);
+		}
+
 	}
 }
