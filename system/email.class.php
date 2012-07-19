ff0644b3f4fc3c3b18df25f3b7ac8868359b220c
Stanislav Starcha <stanislav.starcha@gmail.com>
2012-07-19 16:17:17 +0300

    fixed line breaks for dkim

--- system/email.class.php
+++ system/email.class.php
@@ -4,12 +4,12 @@ class email
 	static public function send($emailTo, $subject, $body)
 	{
 		mb_language('uni');
+		
+		$headers = "Mime-Version: 1.0" . "\r\n";
+		$headers .= "Content-Type: text/html;charset=utf-8" . "\r\n";
+		$headers .= "From: " . conf::$conf['email']['name_from'] . "<" . conf::$conf['email']['email_from'] . ">" . "\r\n";
+		$headers .= "Return-Path: " . conf::$conf['email']['return_path'] . "\r\n";
 
-		$headers = "Mime-Version: 1.0\n";
-		$headers .= "Content-Type: text/html;charset=utf-8\n";
-		$headers .= "From: " . conf::$conf['email']['name_from'] . "<" . conf::$conf['email']['email_from'] . ">\n";
-		$headers .= "Return-Path: " . conf::$conf['email']['return_path'] . "\n";
-
-		return mb_send_mail($emailTo, $subject, $body, $headers, '-f' . conf::$conf['email']['return_path']);
+		return mail($emailTo, $subject, $body, $headers, '-f' . conf::$conf['email']['return_path']);
 	}
 }
