f9b1bac40b04fe0ea614f78eb6a160d576c853b7
Stanislav Starcha <stanislav.starcha@gmail.com>
2012-08-03 18:20:37 +0300

    convert any string to utf8

--- database/db.class.php
+++ database/db.class.php
@@ -95,9 +95,10 @@ class db
 
 	static function _convert($content)
 	{
+		return mb_convert_encoding($content, 'UTF-8');
+
 		if (!mb_check_encoding($content, 'UTF-8') OR !($content === mb_convert_encoding(mb_convert_encoding($content, 'UTF-32', 'UTF-8'), 'UTF-8', 'UTF-32')))
 		{
-			$content = mb_convert_encoding($content, 'UTF-8');
 		}
 
 		return $content;
