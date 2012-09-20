63f0f3f17ebe011a28ac6cefc4db31db52f995a1
Stanislav Starcha <stanislav.starcha@gmail.com>
2012-09-20 03:18:38 +0300

    xhprof type

--- system/profiler.class.php
+++ system/profiler.class.php
@@ -1,25 +1,44 @@
 <?php
 class profiler
 {
-	public static function start()
+	protected static $type = 'default';
+
+	public static function type($type = false)
 	{
-		if (function_exists('xhprof_enable') && conf::$conf['profiler']['enabled'])
+		if ($type)
+		{
+			self::$type = $type;
+		}
+
+		return self::$type;
+	}
+
+	public static function start($forced = false)
+	{
+		if (function_exists('xhprof_enable') && (conf::$conf['profiler']['enabled'] || $forced))
 		{
 			xhprof_enable(XHPROF_FLAGS_MEMORY);
 		}
 	}
 
-	public static function stop()
+	public static function stop($forced = false)
 	{
-		if (function_exists('xhprof_disable') && conf::$conf['profiler']['enabled'])
+		if (function_exists('xhprof_disable') && (conf::$conf['profiler']['enabled'] || $forced))
 		{
 			$data	=	xhprof_disable();
 
 			require_once conf::$conf['rootdir'] . "/lib/plugins/xhprof/xhprof_lib.php";
 			require_once conf::$conf['rootdir'] . "/lib/plugins/xhprof/xhprof_runs.php";
 
-			$runs = new XHProfRuns_Default();
-			$run_id = $runs->save_run($data, "xhprof_foo");
+			$runs 	=	new XHProfRuns_Default();
+			$id		=	$runs->save_run($data, self::type());
+
+			echo "---------------\n".
+				 "Assuming you have set up the http based UI for \n".
+				 "XHProf at some address, you can view run at \n".
+				 "http://<xhprof-ui-address>/index.php?run=$id&source=" . self::type().
+				 "---------------\n";
+
 		}
 	}
 
