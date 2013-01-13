f4e5ab3dfc5a5d5db651200e356cb8790b861df6
Stanislav Starcha <stanislav.starcha@gmail.com>
2013-01-13 20:23:04 +0200

    optional persistent connections

--- database/dbConnection.class.php
+++ database/dbConnection.class.php
@@ -33,13 +33,17 @@ class dbConnection
 
 		try
 		{
-			self::$connections[$alias] = new PDO($uri, $params['user'], $params['password'], array(
+			$options	=	array(
 				PDO::MYSQL_ATTR_INIT_COMMAND 		=> "SET NAMES utf8",
 				PDO::MYSQL_ATTR_USE_BUFFERED_QUERY 	=> true,
-				PDO::ATTR_ERRMODE		=>	PDO::ERRMODE_EXCEPTION,
-				PDO::ATTR_PERSISTENT 	=> true
-			));
-		} catch (PDOException $e)
+				PDO::ATTR_ERRMODE					=>	PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_PERSISTENT 				=> $params['persistent'] ? true : false
+			);
+
+			self::$connections[$alias] = new PDO($uri, $params['user'], $params['password'], $options);
+
+		}
+		catch (PDOException $e)
 		{
 			if ($e->getCode() == '2002')
 			{
