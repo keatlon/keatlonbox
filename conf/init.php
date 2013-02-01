dea29bc885ca50b460090433784f836a93949908
Stanislav Starcha <stanislav.starcha@gmail.com>
2013-02-01 14:09:09 -0800

    - preserve symlinks when building auto-loads

--- conf/init.php
+++ conf/init.php
@@ -1,24 +1,21 @@
 <?php
 
+require_once dirname(__FILE__) . "/../system/sys.php";
+
 /**************************************************************
-* 		DEFINE PATHS & ENVIRONMENT
-***************************************************************/
+ *         DEFINE PATHS & ENVIRONMENT
+ ***************************************************************/
 
-!defined('MODE') ? define('MODE', false ) : false;
+!defined('MODE') ? define('MODE', false) : false;
 
-!defined('ROOTDIR') ?
-		define('ROOTDIR', realpath(dirname(__FILE__) . '/../../') ) : false;
+!defined('ROOTDIR') ? define('ROOTDIR', __realpath(dirname(__FILE__) . '/../..')) : false;
 
-!defined('CONFDIR') ?
-		define('CONFDIR', ROOTDIR . '/conf') : false;
+!defined('CONFDIR') ? define('CONFDIR', ROOTDIR . '/conf') : false;
 
-!defined('PRODUCT') ?
-		(define('PRODUCT', isset($_SERVER['PRODUCT']) ? $_SERVER['PRODUCT'] : 'default')) : false ;
+!defined('PRODUCT') ? (define('PRODUCT', isset($_SERVER['PRODUCT']) ? $_SERVER['PRODUCT'] : 'default')) : false;
 
-!defined('ENVIRONMENT') ?
-		(define('ENVIRONMENT', isset($_SERVER['ENVIRONMENT']) ? $_SERVER['ENVIRONMENT'] : include CONFDIR . "/environment" )) : false ;
+!defined('ENVIRONMENT') ? (define('ENVIRONMENT', isset($_SERVER['ENVIRONMENT']) ? $_SERVER['ENVIRONMENT'] : include CONFDIR . "/environment")) : false;
 
-require_once ROOTDIR . "/core/system/sys.php";
 require_once ROOTDIR . "/core/system/router.class.php";
 
 if (isset($argv))
@@ -32,55 +29,55 @@ class conf
 }
 
 /**************************************************************
-* 		BUILD CONFIG
-***************************************************************/
+ *         BUILD CONFIG
+ ***************************************************************/
 
-$globalConfigFile		= 	dirname(__FILE__) . '/app.global.php';
-$productConfigFile		=	CONFDIR . '/' . PRODUCT . ".all.php";
-$environmentConfigFile	=	CONFDIR . '/' . PRODUCT . '.' . ENVIRONMENT . ".php";
+$globalConfigFile      = dirname(__FILE__) . '/app.global.php';
+$productConfigFile     = CONFDIR . '/' . PRODUCT . ".all.php";
+$environmentConfigFile = CONFDIR . '/' . PRODUCT . '.' . ENVIRONMENT . ".php";
 
-$recompile				=	false;
+$recompile = false;
 
-$metaFile				=	ROOTDIR . '/~cache/conf.meta';
-$jsonFile				=	ROOTDIR . '/~cache/conf.json';
+$metaFile = ROOTDIR . '/~cache/conf.meta';
+$jsonFile = ROOTDIR . '/~cache/conf.json';
 
-$changed				=	max(filemtime($globalConfigFile), filemtime($productConfigFile), filemtime($environmentConfigFile));
-$compiled				=	file_get_contents($metaFile);
+$changed  = max(filemtime($globalConfigFile), filemtime($productConfigFile), filemtime($environmentConfigFile));
+$compiled = file_get_contents($metaFile);
 
 if (!file_exists($metaFile) || !file_exists($jsonFile) || ($changed > $compiled))
 {
-	$recompile			=	true;
+    $recompile = true;
 }
 
 if ($recompile)
 {
-	$globalConfig		= 	include $globalConfigFile;
-	$productConfig		=	include $productConfigFile;
-	$environmentConfig	=	include $environmentConfigFile;
+    $globalConfig      = include $globalConfigFile;
+    $productConfig     = include $productConfigFile;
+    $environmentConfig = include $environmentConfigFile;
 
-	$config				=	_amr($globalConfig, $productConfig);
-	conf::$conf			=	_amr($config, $environmentConfig);
+    $config     = _amr($globalConfig, $productConfig);
+    conf::$conf = _amr($config, $environmentConfig);
 
-	file_put_contents($jsonFile, json_encode(conf::$conf));
-	file_put_contents($metaFile, $changed);
+    file_put_contents($jsonFile, json_encode(conf::$conf));
+    file_put_contents($metaFile, $changed);
 }
 else
 {
-	conf::$conf	= 	json_decode(file_get_contents($jsonFile), true);
+    conf::$conf = json_decode(file_get_contents($jsonFile), true);
 }
 
-$application	=	isset($arguments['application']) ? $arguments['application'] : false;
+$application = isset($arguments['application']) ? $arguments['application'] : false;
 
 if (!$application)
 {
-	$application	=	isset($_SERVER['APPLICATION']) ? $_SERVER['APPLICATION'] : 'main';
+    $application = isset($_SERVER['APPLICATION']) ? $_SERVER['APPLICATION'] : 'main';
 }
 
 !defined('APPLICATION') ? define('APPLICATION', $application) : false;
 
 foreach (conf::$conf['phpini'] as $key => $value)
 {
-	ini_set($key, $value);
+    ini_set($key, $value);
 }
 
 router::init(APPLICATION ? APPLICATION : false);
--- system/sys.php
+++ system/sys.php
@@ -1,5 +1,32 @@
 <?php
 
+function __realpath($path)
+{
+    $path      = str_replace(array(
+        '/',
+        '\\'
+    ), DIRECTORY_SEPARATOR, $path);
+
+    $parts     = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
+    $absolutes = array();
+    foreach ($parts as $part)
+    {
+        if ('.' == $part) {
+            continue;
+        }
+        if ('..' == $part)
+        {
+            array_pop($absolutes);
+        }
+        else
+        {
+            $absolutes[] = $part;
+        }
+    }
+
+    return implode(DIRECTORY_SEPARATOR, $absolutes);
+}
+
 function __parseArguments($arguments)
 {
     if (!$arguments)
